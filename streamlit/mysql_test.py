2a01c6e5d9627b5258b58005558c361cee798796
jrwblackwell <121980359+jrwblackwell@users.noreply.github.com>
2023-09-14 17:15:57 -0400

    Update mysql_test.py

--- streamlit/mysql_test.py
+++ streamlit/mysql_test.py
@@ -1,6 +1,13 @@
+import pandas as pd
 import streamlit as st
 
+conn = st.experimental_connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * FROM PO_LINE;', ttl=600)
 
 st.title('MySQL Streamlit App Test') 
 
 st.write('not exactly sure what is going on here')
+
+st.write(df.shape)
