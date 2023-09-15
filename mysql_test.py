69bd2ec0e2fdea04f77e82c924925b09245e4e51
jrwblackwell <121980359+jrwblackwell@users.noreply.github.com>
2023-09-15 14:22:37 -0400

    Update mysql_test.py

--- mysql_test.py
+++ mysql_test.py
@@ -2,11 +2,11 @@ import pymysql
 import pandas as pd
 import streamlit as st
 
-connection = pymysql.connect(host='127.0.0.1',
+connection = pymysql.connect(host='localhost',
                             port=3306,
                             user='root',
                             passwd='gemini98',
-                            db='Sales')
+                            db='test_data')
 
 sql_query = f'SELECT * FROM salary_journey;' 
 
