81f6ba795856ce9d14355c051de335ff3ee2ce2c
jrwblackwell <121980359+jrwblackwell@users.noreply.github.com>
2023-09-15 12:15:55 -0400

    Update mysql_test.py

--- mysql_test.py
+++ mysql_test.py
@@ -1,38 +1,65 @@
-import mysql.connector as connection
 import pandas as pd
 import streamlit as st
-import toml
 
-# read credentials
-toml_data = toml.load("secrets.toml")
+connection = pymysql.connect(host='127.0.0.1',
+                            port=3306,
+                            user='root',
+                            passwd='gemini98',
+                            db='Sales',
+                            cursorclass=pymysql.cursors.DictCursor)
 
-# assign each credential to a variable
-HOST_NAME = toml_data['mysql']['host']
-DATABASE = toml_data['mysql']['database']
-PASSWORD = toml_data['mysql']['password']
-USER = toml_data['mysql']['user']
-PORT = toml_data['mysql']['port']
+sql_query = f'SELECT * FROM Shippers' 
 
-# connect to database
-db = connection.connect(host=HOST_NAME, database=DATABASE, user=USER, passwd=PASSWORD, use_pure=True)
+with connection.cursor() as cursor:
+    cursor.execute(sql_query)
 
-#generate dataframe
-df = pd.read_sql('SELECT * FROM Shippers' , db)
+    # Connection is not autocommit by default, so we must commit to save changes
+    connection.commit()
+    
+    # Fetch all the records from SQL query output
+    results = cursor.fetchall()
+    
+    # Convert results into pandas dataframe
+    df = pd.DataFrame(results)                                     
 
-# configure page 
-st.set_page_config(
-    page_title="The Best Portfolio They Will EVER See",
-    layout="wide"
-)
+st.write(df.shape)
 
-# title of app
-# st.title('')
 
-# display dataframe
-st.write(df)
+#import mysql.connector as connection
+# import pandas as pd
+# import streamlit as st
+# import toml
 
-with st.sidebar:
-    st.write('This is the side bar!')
+# # read credentials
+# toml_data = toml.load("secrets.toml")
+
+# # assign each credential to a variable
+# HOST_NAME = toml_data['mysql']['host']
+# DATABASE = toml_data['mysql']['database']
+# PASSWORD = toml_data['mysql']['password']
+# USER = toml_data['mysql']['user']
+# PORT = toml_data['mysql']['port']
+
+# # connect to database
+# db = connection.connect(host=HOST_NAME, database=DATABASE, user=USER, passwd=PASSWORD, use_pure=True)
+
+# #generate dataframe
+# df = pd.read_sql('SELECT * FROM Shippers' , db)
+
+# # configure page 
+# st.set_page_config(
+#     page_title="The Best Portfolio They Will EVER See",
+#     layout="wide"
+# )
+
+# # title of app
+# # st.title('')
+
+# # display dataframe
+# st.write(df)
+
+# with st.sidebar:
+#     st.write('This is the side bar!')
 
 # conn = st.experimental_connection('mysql', type='sql')
 
