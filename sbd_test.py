bcbd1ecfd1784979a2efe9a6fc0b830b3ebc079c
jrwblackwell <121980359+jrwblackwell@users.noreply.github.com>
2023-09-15 22:54:18 -0400

    Create sbd_test.py

---
+++ sbd_test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from st_supabase_connection import SupabaseConnection
+
+# Initialize connection.
+conn = st.experimental_connection("supabase",type=SupabaseConnection)
+
+# Perform query.
+rows = conn.query("*", table="salary", ttl="10m").execute()
+
+st.write(rows)
